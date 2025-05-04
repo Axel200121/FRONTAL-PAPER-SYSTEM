f73021360479f8ca1c794695d361c8791666d324
Axel200121 <axelmartinez212001@gmail.com>
2025-05-03 18:11:20 -0600

    configurando primeng y talwindcss

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,24 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+    provideAnimationsAsync(),
+        providePrimeNG({
+            theme: {
+                preset: Aura,
+                options: { // este es para agregar cosas más avanzadas
+                  cssLayer: {
+                      name: 'primeng',
+                      order: 'theme, base, primeng'
+                  }
+              }
+            }
+        })
+  ]
 };

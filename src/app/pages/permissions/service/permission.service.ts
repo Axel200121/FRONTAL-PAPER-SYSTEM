6805c8b8c9caea8ed5d7c98c47822fc3ad6863fc
Axel200121 <axelmartinez212001@gmail.com>
2025-05-09 18:55:22 -0600

    creación folder permisos  y serivicio permisos

---
+++ src/app/pages/permissions/service/permission.service.ts
@@ -0,0 +1,55 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment.development';
+import { PermissionDto } from '../../../interfaces/permission.dto';
+import { Observable } from 'rxjs';
+import { ApiResponseDto } from '../../../interfaces/api.response.dto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PermissionService {
+
+
+  private http = Inject(HttpClient)
+  private url = environment.apiUrl
+  constructor() { }
+
+
+  public executeSavePermission(permissionDto:PermissionDto):Observable<ApiResponseDto>{
+    const url = `${this.url}/permissions/save`
+    return this.http.post(url,permissionDto)
+  }
+
+  public executeUpdatePermission(id:string, permissionDto:PermissionDto):Observable<ApiResponseDto>{
+    const url = `${this.url}/permissions/update/${id}`
+    return this.http.put(url, permissionDto);
+  }
+
+  public executeListPermissions(page:number, size:number, status?:string, idPermission?:string):Observable<any>{
+    let params = new HttpParams()
+    .set('page', page.toString())
+    .set('size', size.toString())
+
+    if (status || idPermission) {
+      params = params
+      .set('status', status!.toString())
+      .set('idPermission', idPermission!.toString())
+    }
+
+    const url = `${this.url}/permissions/alls`
+    return this.http.get(url,{params})
+  }
+
+  public executeGetRole(id:string):Observable<ApiResponseDto>{
+    const url = `${this.url}/permissions/get/${id}`
+    return this.http.get(url)
+  }
+
+  public executeDeletePermission(id:string):Observable<ApiResponseDto>{
+    const url = `${this.url}/permissions/delete/${id}`
+    return this.http.delete(url)
+  }
+
+
+}
